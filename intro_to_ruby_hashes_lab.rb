b76236d519084c5c4f097f94f48d46dfe4858429
Ruari Day <ruariday@gmail.com>
2019-11-29 16:43:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,8 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  base_hash = {:railroads => { 
+  base_hash = {
+    :railroads => { 
     :pieces => 4, 
     :rent_in_dollars => {
       :one_piece_owned => 25,
