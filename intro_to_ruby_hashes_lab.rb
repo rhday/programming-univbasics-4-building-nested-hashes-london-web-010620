8a1e4d5f69cc95f51eb2b7de76c441692a5f3865
Ruari Day <ruariday@gmail.com>
2019-11-29 16:33:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,8 @@ def monopoly_with_fourth_tier
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
-        :
+        :b_and_o_railroad => {},
+        :shortline => {}
     }
   }
 end
