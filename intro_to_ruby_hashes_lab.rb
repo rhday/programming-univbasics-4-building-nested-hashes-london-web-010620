457f7016ecb505d1d3d00ab4dedbe53aa22901c2
Ruari Day <ruariday@gmail.com>
2019-11-29 16:49:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,9 +20,9 @@ def monopoly_with_third_tier
       :three_pieces_owned =>100, 
       :four_pieces_owned => 200},
     :names => {
-        :reading_railroad => {"mortgage_value"=>"$100"},
-        :pennsylvania_railroad => {"mortgage_value"=>"$200"},
-        :b_and_o_railroad => {"mortgage_value"=>"$400"},
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
         :shortline_railroad => {} 
       }
     }
@@ -43,9 +43,9 @@ def monopoly_with_fourth_tier
       :three_pieces_owned =>100, 
       :four_pieces_owned => 200},
     :names => {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
+        :reading_railroad => {"mortgage_value"=>"$100"},
+        :pennsylvania_railroad => {"mortgage_value"=>"$200"},
+        :b_and_o_railroad => {"mortgage_value"=>"$400"},
         :shortline_railroad => {"mortgage_value"=>"$800"} 
       }
     }
