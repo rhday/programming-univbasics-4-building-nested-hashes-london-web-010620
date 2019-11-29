27ddb0544d5332aa1811c0bf058ba562d7c59f24
Ruari Day <ruariday@gmail.com>
2019-11-29 16:48:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
         :reading_railroad => {"mortgage_value"=>"$100"},
         :pennsylvania_railroad => {"mortgage_value"=>"$200"},
         :b_and_o_railroad => {"mortgage_value"=>"$400"},
-        :shortline_railroad => {} 
+        :shortline_railroad => {"mortgage_value"=>"$800"} 
       }
     }
   }
